9bb95961acc39f8e31099dd923fc9cc31326d6d2
Yusuke DOI <doi@wide.ad.jp>
2013-06-01 16:53:25 +0900

    Added XML samples and corresponding EXI streams (however, the streams are not good because the schema is not yet fixed.) Added some examples on initial negotiations of alternate transport (again, EXI is not good until the schema is fixed.)

---
+++ samples/make_exi_samples.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+EXIFICIENT_DIR=/shome/ydoi/.s/exificient-sep2work
+WDIR=`/bin/pwd`
+SCHEMA_DIR=$WDIR/../schema/LOCAL/
+
+BASESCHEMA=$SCHEMA_DIR/base.xsd
+EXTSCHEMA=$SCHEMA_DIR/base+muc.xsd
+
+cd $EXIFICIENT_DIR
+. ./env.sh
+cd src/sample
+export EXI_STRICT=1
+export EXI_USE_OPTION=0
+
+for TARGET in C2S S2C; do
+ if [ x`ls $WDIR/$TARGET/|grep '\.exi$'|wc -l` != 'x0' ]; then
+  echo "please cleanup $TARGET (remove exi files)"
+  exit 1
+ fi
+ ENC_MODE=normal
+ EXI_PERSISTENT_CONTEXT=0 jython loopEncoder.py $BASESCHEMA $WDIR/$TARGET/00*.xml
+ [ -d $WDIR/exi-$TARGET-$ENC_MODE/base ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base
+ mv -f $WDIR/$TARGET/00*.exi $WDIR/exi-$TARGET-$ENC_MODE/base/
+ EXI_PERSISTENT_CONTEXT=0 jython loopEncoder.py $EXTSCHEMA $WDIR/$TARGET/*.xml
+ [ -d $WDIR/exi-$TARGET-$ENC_MODE/base+muc ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base+muc
+ #[ -d $WDIR/exi-$TARGET-$ENC_MODE/base+muc-dummy ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base+muc-dummy
+ #mv -f $WDIR/$TARGET/00*.exi $WDIR/exi-$TARGET/base+muc-dummy/
+ rm -f $WDIR/$TARGET/00*.exi
+ mv -f $WDIR/$TARGET/*.exi $WDIR/exi-$TARGET-$ENC_MODE/base+muc/
+
+ ENC_MODE=persistent
+ EXI_PERSISTENT_CONTEXT=1 jython loopEncoder.py $BASESCHEMA $WDIR/$TARGET/00*.xml
+ [ -d $WDIR/exi-$TARGET-$ENC_MODE/base ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base
+ mv -f $WDIR/$TARGET/00*.exi $WDIR/exi-$TARGET-$ENC_MODE/base/
+ EXI_PERSISTENT_CONTEXT=1 jython loopEncoder.py $EXTSCHEMA $WDIR/$TARGET/*.xml
+ [ -d $WDIR/exi-$TARGET-$ENC_MODE/base+muc ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base+muc
+ [ -d $WDIR/exi-$TARGET-$ENC_MODE/base+muc-dummy ] || mkdir -p $WDIR/exi-$TARGET-$ENC_MODE/base+muc-dummy
+ mv -f $WDIR/$TARGET/00*.exi $WDIR/exi-$TARGET-$ENC_MODE/base+muc-dummy/
+ mv -f $WDIR/$TARGET/*.exi $WDIR/exi-$TARGET-$ENC_MODE/base+muc/
+
+done
